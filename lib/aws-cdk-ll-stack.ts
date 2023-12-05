003a91f950ca28eb6856d64a490c61cd520d0005
zachbeeler <zach@scalesec.com>
2023-12-05 13:34:59 -0600

    changing to code star connection

--- lib/aws-cdk-ll-stack.ts
+++ lib/aws-cdk-ll-stack.ts
@@ -10,7 +10,9 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('zachbeeler/aws-cdk-ll', 'main'),
+        input: CodePipelineSource.connection('zachbeeler/aws-cdk-ll', 'main', {
+          connectionArn: 'arn:aws:codestar-connections:us-east-2:268548213468:connection/c6ef8d1b-6212-40f8-8e60-02383520bd73'
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
