7dc68290f1ee99ec1afe1dbbec137ffe87444ed3
zachbeeler <zach@scalesec.com>
2023-12-05 13:24:34 -0600

    adding repo owner

--- lib/aws-cdk-ll-stack.ts
+++ lib/aws-cdk-ll-stack.ts
@@ -10,7 +10,7 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('zachbeeler/aws-cdk-ll', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
